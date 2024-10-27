9113ed16243359a38ab727004076d037bb90323e
karthikkuddigana <63944339+Karthikkuddigana@users.noreply.github.com>
2024-10-27 21:07:36 +0530

    Added merge sort implementation with minor errors

--- script.js
+++ script.js
@@ -123,3 +123,51 @@
         console.log(message)
     }); 
  })
+ let arr1=[1,8]; 
+ let arr2=[3,9]; 
+function sort(arr1,arr2){
+    sortedArray=[]; 
+    for(let i=0; i<arr1.length; i++){
+        for(let j=0; j<arr2.length; j++){
+            if(!sortedArray.includes(arr1[i]) && !sortedArray.includes(arr2[j])){
+                if(arr1[i]<arr2[j]){
+                    console.log(arr1[i])
+                    sortedArray.push(arr1[i])
+                    console.log(sortedArray)
+                    if(i==arr1.length-1 && !sortedArray.includes(arr2[j])){
+                        sortedArray.push(arr2[j])
+                    }
+                }
+                else{
+                    // console.log(arr2[j])
+                    sortedArray.push(arr2[i])
+                    if(i==arr1.length-1 && !sortedArray.includes(arr1[j])){
+                        sortedArray.push(arr1[j])
+                    }
+                    // console.log(sortedArray)
+                }
+            }
+        }
+    }
+    console.log(sortedArray)
+    return sortedArray
+}
+sort(arr1,arr2)
+//  function MergeSort(myArr){
+//     // console.log(myArr)
+//     if(myArr.length==1){
+//         return myArr
+//     }
+//     else{
+//         let n=myArr.length; 
+//         // console.log(myArr); 
+//         let arr1=myArr.slice(0,n/2); 
+//         let arr2=myArr.slice(n/2,n); 
+//         console.log(arr1); 
+//         console.log(arr2); 
+//         return sort(MergeSort(arr1),MergeSort(arr2))
+//     }
+//  }
+// let array1=[7,4,5,2,1,8,3,9]; 
+//  console.log(MergeSort(array1)); 
+//  console.log(ansArray); 
